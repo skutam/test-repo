923c5ec68390c1a4dfb02c3e1e2e7c34adb02070
skutam <43553962+skutam@users.noreply.github.com>
2024-08-22 22:53:07 +0000

    Deploying to gh-pages from @ skutam/test-repo@c5080da687ef6d23c6a8dbd62efef33da2f52666 🚀

---
+++ my-lib/esm2022/lib/my-lib.module.mjs
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { MyLibComponent } from './my-lib.component';
+import * as i0 from "@angular/core";
+export class MyLibModule {
+    static { this.ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "16.2.12", ngImport: i0, type: MyLibModule, deps: [], target: i0.ɵɵFactoryTarget.NgModule }); }
+    static { this.ɵmod = i0.ɵɵngDeclareNgModule({ minVersion: "14.0.0", version: "16.2.12", ngImport: i0, type: MyLibModule, declarations: [MyLibComponent], exports: [MyLibComponent] }); }
+    static { this.ɵinj = i0.ɵɵngDeclareInjector({ minVersion: "12.0.0", version: "16.2.12", ngImport: i0, type: MyLibModule }); }
+}
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "16.2.12", ngImport: i0, type: MyLibModule, decorators: [{
+            type: NgModule,
+            args: [{
+                    declarations: [
+                        MyLibComponent
+                    ],
+                    imports: [],
+                    exports: [
+                        MyLibComponent
+                    ]
+                }]
+        }] });
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibXktbGliLm1vZHVsZS5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uLy4uLy4uL3Byb2plY3RzL215LWxpYi9zcmMvbGliL215LWxpYi5tb2R1bGUudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsT0FBTyxFQUFFLFFBQVEsRUFBRSxNQUFNLGVBQWUsQ0FBQztBQUN6QyxPQUFPLEVBQUUsY0FBYyxFQUFFLE1BQU0sb0JBQW9CLENBQUM7O0FBY3BELE1BQU0sT0FBTyxXQUFXOytHQUFYLFdBQVc7Z0hBQVgsV0FBVyxpQkFScEIsY0FBYyxhQUtkLGNBQWM7Z0hBR0wsV0FBVzs7NEZBQVgsV0FBVztrQkFWdkIsUUFBUTttQkFBQztvQkFDUixZQUFZLEVBQUU7d0JBQ1osY0FBYztxQkFDZjtvQkFDRCxPQUFPLEVBQUUsRUFDUjtvQkFDRCxPQUFPLEVBQUU7d0JBQ1AsY0FBYztxQkFDZjtpQkFDRiIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IE5nTW9kdWxlIH0gZnJvbSAnQGFuZ3VsYXIvY29yZSc7XG5pbXBvcnQgeyBNeUxpYkNvbXBvbmVudCB9IGZyb20gJy4vbXktbGliLmNvbXBvbmVudCc7XG5cblxuXG5ATmdNb2R1bGUoe1xuICBkZWNsYXJhdGlvbnM6IFtcbiAgICBNeUxpYkNvbXBvbmVudFxuICBdLFxuICBpbXBvcnRzOiBbXG4gIF0sXG4gIGV4cG9ydHM6IFtcbiAgICBNeUxpYkNvbXBvbmVudFxuICBdXG59KVxuZXhwb3J0IGNsYXNzIE15TGliTW9kdWxlIHsgfVxuIl19
---
+++ my-lib/lib/my-lib.component.d.ts
@@ -0,0 +1,5 @@
+import * as i0 from "@angular/core";
+export declare class MyLibComponent {
+    static ɵfac: i0.ɵɵFactoryDeclaration<MyLibComponent, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<MyLibComponent, "lib-my-lib", never, {}, {}, never, never, false, never>;
+}
