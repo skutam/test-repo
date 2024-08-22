3fe314965e3585acb182664586cb0ea31b5cd8ff
skutam <43553962+skutam@users.noreply.github.com>
2024-08-22 22:59:06 +0000

    Deploying to gh-pages from @ skutam/test-repo@352a69f3cf395ffed380340c8be75ef452a956df 🚀

--- my-lib/lib/my-lib.component.d.ts
+++
@@ -1,5 +0,0 @@
-import * as i0 from "@angular/core";
-export declare class MyLibComponent {
-    static ɵfac: i0.ɵɵFactoryDeclaration<MyLibComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<MyLibComponent, "lib-my-lib", never, {}, {}, never, never, false, never>;
-}
